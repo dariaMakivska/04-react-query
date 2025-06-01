257da3a3790972f06ffddbb2151646e18a2f17f4
Daria Makivska <peony.bluet@gmail.com>
2025-06-01 20:53:01 +0300

    Налаштовує Reeact-query та React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import 'modern-normalize/modern-normalize.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App/App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
